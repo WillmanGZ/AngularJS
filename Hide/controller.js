80f9b5e9b36bb012131191ff785931014b69cc91
WillmanGZ <willmang@uninorte.edu.co>
2025-10-14 08:32:51 -0500

    feat: new app using ng show and ng hide

---
+++ Hide/controller.js
@@ -0,0 +1,18 @@
+angular
+  .module("MyFirstApp", [])
+  .controller("FirstController", function ($scope, $http) {
+    $scope.posts = [];
+    $scope.loading = true;
+
+    $http.get("https://jsonplaceholder.typicode.com/posts").then(
+      function (data) {
+        console.log(data);
+        $scope.posts = data.data;
+        $scope.loading = false;
+      },
+      function (error) {
+        console.log(error);
+        $scope.loading = false;
+      }
+    );
+  });
